96f1d2aa66f38222c9188161c827adf8ddb7d9a3
hogehoge <hogehoge@gmail.com>
2024-05-22 23:11:58 +0900

    update_system.sh

--- Setup/update_system.sh
+++ Setup/update_system.sh
@@ -10,3 +10,9 @@ sudo timedatectl set-timezone Asia/Tokyo
 echo net-toolsインストール
 # ifconfigなどを使えるように
 sudo apt install net-tools
+
+# 日本語の言語パッケージを適応
+sudo apt install language-pack-ja language-pack-ja-base
+locale -a
+sudo update-locale LANG=ja_JP.UTF-8
+locale -a
